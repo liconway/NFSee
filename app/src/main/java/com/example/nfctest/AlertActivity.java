f6478a93df4cfac828a7af37de0c9ed83cc08343
Derek Braun <derek.braun.99@gmail.com>
2019-02-16 23:04:20 -0700

    Added Alerts Activity

--- app/src/main/java/com/example/nfctest/AlertActivity.java
+++ app/src/main/java/com/example/nfctest/AlertActivity.java
@@ -1,41 +1,27 @@
 package com.example.nfctest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class Favorites extends AppCompatActivity {
+public class AlertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorites);
+        setContentView(R.layout.activity_alert);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.backToMainButton);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-            }
-        });
-
-        configureBackButton();
-    }
-
-    private void configureBackButton(){
-        FloatingActionButton backButton = (FloatingActionButton) findViewById(R.id.backToMainButton);
-        backButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(Favorites.this, MainActivity.class));
             }
         });
     }
