8b3737d0aabcc90675cdc263f743e3d124c90918
Derek Braun <derek.braun.99@gmail.com>
2019-02-17 07:54:07 -0700

    Added connections to urls

--- app/src/main/java/com/example/nfctest/BigBoyActivity.java
+++ app/src/main/java/com/example/nfctest/BigBoyActivity.java
@@ -3,11 +3,13 @@ package com.example.nfctest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -17,6 +19,7 @@ import org.json.JSONObject;
 public class BigBoyActivity extends AppCompatActivity {
     ViewPager viewPager;
     SlideAdapter adapter;
+    JSONObject json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,17 @@ public class BigBoyActivity extends AppCompatActivity {
 
         System.out.println("Big Boy is Running!!");
 
-        JSONObject json = getJSON();
+        json = getJSON();
+    }
+
+    private void openTripAdvisor(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(json.optString("taurl")));
+        startActivity(browserIntent);
+    }
+
+    private void openYelp(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(json.optString("yurl")));
+        startActivity(browserIntent);
     }
 
     private Bitmap[] getBitmaps(String[] urls) {
